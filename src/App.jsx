1e15b8550a3686b8d6316454a7aabfab6b08f5f7
Keila Amada <keila.amadap@hotmail.com>
2023-08-12 12:48:48 -0300

    importacao corrigida

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
         </Routes>
 
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
     </div>
   );
